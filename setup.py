0b56dec3fd1dfe90a18fba7dc7b925dff7d29641
Sinesipo Mbasa <Sinesipo@explore-datascience.net>
2019-03-19 16:19:23 +0200

    Just for control

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='package',
+    name='myPack',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/Sinesipo/hackathon.git',
+    url='https://github.com/Sinesipo/myPack.git',
     author='Sinesipo Mbasa',
     author_email='smbasa1818@gmail.com'
 )
